af153f5563808603c1757979ac6a6c1c33458f90
Itamar Silva Rocha dos Santos <itamar.silva@sonda.com>
2018-10-09 12:46:48 -0300

    commit initial

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
 # Overview #
 
-The built in Build Breaker Plugin for SonarQube has retired in 5.1 release. This is a workaround to achieve the same using SonarQube REST APIs.
+O plugin para análise de resultado do SonarQube tem várias funcionalidade, como para um build de acordo com o status do projeto na integração contínua. Ele utiliza o Sonarqube REST APIs para extrair os dados.
 
 # Usage #
-python SonarBuildBreaker.py <sonar_server_url> <groupId> <artifactId> <branch> <mode>
+python SonarBuildBreaker.py <sonar_server_url> <projectKey> 
 
-Mode (1 or 2)
+# Evolution #
 
-*   1 - Pre-Quality Analysis. This has to be execute before Sonar analysis to fetch the existing quality metrics from Sonar server
-*   2 - Post-Quality Analysis. This has to be execute after Sonar analysis to fetch the latest quality metrics from Sonar server and compare with the previous metrics
-
-e.g. python SonarBuildBreaker.py 'http://sonar.dev.intsys.atlassian.com:9000' 'com.atlassian.platform' 'ctk-plugin' master 2
-
-By default, the build will fail if any increase in issues of type BLOCKER, CRITICAL or MAJOR. 
-But you can control the behaviour by setting these environment variables **skipBlocker, skipCritical, skipMajor, skipMinor, skipInfo**
-
-# Reference Bamboo Plan #
-https://ecosystem-bamboo.internal.atlassian.com/browse/NRAM-SQPLUG
-
-# Limitations
-* This will fail the build if we run first time (on master or any branch build)because the pre-analyser return ZERO for all issue types.
+Nos ajude a evoluir a partir deste script, faça um fork e commit novas funcionalidades, vamos usar o poder do python para agilizar e facilitar a vida dos devs
--- SonarBuildBreaker.py
+++ SonarBuildBreaker.py
@@ -1,8 +1,8 @@
-import urllib, json, re, subprocess, sys, os
-import ConfigParser
+import requests, json, re, subprocess, sys, os
 
 class Sonar:
 	sonar_server = ""
+	key = ""
 	total_issues = 0
 	total_blockers = 0
 	total_critical = 0
@@ -10,23 +10,28 @@ class Sonar:
 	total_minor = 0
 	total_info = 0
 
-	def __init__(self, sonar_server):
+	def __init__(self, sonar_server, key):
 		self.sonar_server = sonar_server
+		self.key = key
 
-	def getTotalResponsePages(self, groupId, artifactId, branch):
-		url = self.sonar_server+"/api/issues/search?componentRoots="+groupId+":"+artifactId+":"+branch
-		data = json.loads(urllib.urlopen(url).read())
+	def getTotalResponsePages(self, key):
+		url = self.sonar_server+"/api/issues/search?id="+key
+		r = requests.get(url)
+		
+		data = json.loads(r.text)
 		self.total_issues = data['total']
 		return data['paging']['total']
 
-	def findIssues(self, groupId, artifactId, branch):
-		print "Finding issues from SonarQube"
-		url = self.sonar_server+"/api/issues/search?componentRoots="+groupId+":"+artifactId+":"+branch
-		self.totalPages = self.getTotalResponsePages(groupId, artifactId, branch)
+	def findIssues(self, key):
+		print ("Finding issues from SonarQube") 
+		url = self.sonar_server+"/api/issues/search?id="+key
+		
+		self.totalPages = self.getTotalResponsePages(key)
 		pageIndex=1
 		while (pageIndex <= self.totalPages):
-			url2 = url+"&pageIndex="+str(pageIndex)		
-			data = json.loads(urllib.urlopen(url2).read())
+			url2 = url+"&pageIndex="+str(pageIndex)	
+			r = requests.get(url2)	
+			data = json.loads(r.text)
 			self.total_blockers += len(re.findall("BLOCKERS", str(data)))
 			self.total_critical += len(re.findall("CRITICAL", str(data)))
 			self.total_major += len(re.findall("MAJOR", str(data)))
@@ -35,121 +40,35 @@ class Sonar:
 			pageIndex+=1
 
 	def printResults(self):
-		print "\n"
-		print "Total Issues   : ",self.total_issues
-		print "Total Blcokers : ",self.total_blockers
-		print "Total Critical : ",self.total_critical
-		print "Total Majors   : ",self.total_major
-		print "Total Minor    : ",self.total_minor
-		print "Total Info     : ",self.total_info
-		print "\n"
-
-	def writeResults(self):
-		print "Writing results to file"
-		subprocess.call(["touch", SONAR_RESULTS])
-		f = open(SONAR_RESULTS,'w')
-		try:
-			f.write('[Default]\n')
-			f.write('total = '+str(self.total_issues)+'\n')
-			f.write('blocker = '+str(self.total_blockers)+'\n')
-			f.write('critical = '+str(self.total_critical)+'\n')
-			f.write('major = '+str(self.total_major)+'\n')
-			f.write('minor = '+str(self.total_minor)+'\n')
-			f.write('info = '+str(self.total_info)+'\n')
-		except IOError:
-			print "Error while writing to file"
-		finally:
-			f.close()
-
-	def readResults(self):
-		print "Reading previous results from file"
-		config = ConfigParser.RawConfigParser()
-		config.read(SONAR_RESULTS)
-		self.total_issues = int(config.get('Default', 'total'))
-		self.total_blockers = int(config.get('Default', 'blocker'))
-		self.total_critical = int(config.get('Default', 'critical'))
-		self.total_major = int(config.get('Default', 'major'))
-		self.total_minor = int(config.get('Default', 'minor'))
-		self.total_info = int(config.get('Default', 'info'))
+		print ("\n")
+		print ("Total Issues   : ",self.total_issues)
+		print ("Total Blcokers : ",self.total_blockers)
+		print ("Total Critical : ",self.total_critical)
+		print ("Total Majors   : ",self.total_major)
+		print ("Total Minor    : ",self.total_minor)
+		print ("Total Info     : ",self.total_info)
+		print ("\n")
 
 
-SONAR_RESULTS = "sonarResults.out"
+print ("Sonar URL   : ", sys.argv[1])
+print ("Key   : ", sys.argv[2])
+print ("\n")
+print ("Analysis")
 
-if (len(sys.argv) < 6):
-	print "Usage : python SonarBuildBreaker.py <sonar_server_url> <groupId> <artifactId> <branch> <mode>"
-	print "Mode (1 or 2)"
-	print "\t 1. Pre-Quality Analysis"
-	print "\t 2. Post-Quality Analysis"
-	exit(1)
+post = Sonar(sys.argv[1], sys.argv[2])
+post.findIssues(sys.argv[2])
+post.printResults()
 
-print "Sonar URL   : ", sys.argv[1]
-print "Group Id    : ", sys.argv[2]
-print "Artifact id : ", sys.argv[3]
-print "Branch      : ", sys.argv[4]
-print "Mode        : ", sys.argv[5]
-print "\n"
+url = sys.argv[1]+"/api/qualitygates/project_status?projectKey="+sys.argv[2]
+r = requests.get(url)	
+parsed_json = json.loads(r.text)
+gate = parsed_json['projectStatus']['status']
 
-if (sys.argv[5] == '1'):
-	print ("Runnig in PRE Analysis mode")
-	pre = Sonar (sys.argv[1])
-	pre.findIssues (sys.argv[2], sys.argv[3], sys.argv[4])
-	pre.printResults()
-	pre.writeResults()
 
-elif (sys.argv[5] == '2'):
-	print ("Runnig in POST Analysis mode")
-	post = Sonar (sys.argv[1])
-	post.findIssues (sys.argv[2], sys.argv[3], sys.argv[4])
-	post.printResults()
-
-	pre = Sonar (sys.argv[1])
-	pre.readResults()
-	pre.printResults()
-
-	print "Running COMPARISON"
-	msg = ''
-	fail = False
-	
-	skipBlocker = os.getenv('skipBlocker', False)
-	skipCritical = os.getenv('skipCritical', False)
-	skipMajor = os.getenv('skipMajor', True)
-	skipMinor = os.getenv('skipMinor', True)
-	skipInfo = os.getenv('skipInfo', True)
-
-	if (pre.total_issues < post.total_issues):
-		msg = "New issues "+ str(post.total_issues - pre.total_issues)+"\n"
-
-	if (pre.total_blockers < post.total_blockers):
-		msg = msg + "New BLOCKER issues "+ str(post.total_blockers - pre.total_blockers)+"\n"
-		if(not skipBlocker):
-			fail = True
-
-	if (pre.total_critical < post.total_critical):
-		msg = msg + "New CRITICAL issues "+ str(post.total_critical - pre.total_critical)+"\n"
-		if(not skipCritical):
-			fail = True
-
-	if (pre.total_major < post.total_major):
-		msg = msg + "New MAJOR issues "+ str(post.total_major - pre.total_major)+"\n"
-		if(not skipMajor):
-			fail = True
-
-	if (pre.total_minor < post.total_minor):
-		msg = msg + "New MINOR issues "+ str(post.total_minor - pre.total_minor)+"\n"
-		if(not skipMinor):
-			fail = True
-
-	if (pre.total_info < post.total_info):
-		msg = msg + "New INFO issues "+ str(post.total_info - pre.total_info)+"\n"
-		if(not skipInfo):
-			fail = True
-
-	if (fail):
-		print "Analysis result : FAILED"
-		print msg
-		print "Refer report : http://sonar.dev.intsys.atlassian.com:9000/?did=11"
-		exit (1)
-	else:
-		print "Analysis result : PASSED"
-		print msg
-		print "Refer report : http://sonar.dev.intsys.atlassian.com:9000/?did=11"
+if (gate == 'ERROR'):
+	print ("Analysis result : FAILED")
+	print ("Refer report :" +sys.argv[1]+"/api/issues/search?id="+sys.argv[2])
+	exit (1)
+else:
+	print ("Analysis result : PASSED")
+	print ("Refer report : "+sys.argv[1]+"/api/issues/search?id="+sys.argv[2])
